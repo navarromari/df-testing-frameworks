0ce42bdc8e97fdb492c6c7abf5cc97a279fa6248
Mariana Navarro <mariana.navarrogoes@gmail.com>
2023-01-15 19:28:00 +0000

    test frameworks further work - improved spec code

--- spec/countEvenNumbers.spec.js
+++ spec/countEvenNumbers.spec.js
@@ -1,44 +1,33 @@
 const { assertEquals } = require(`../test-framework.js`); //destructuring assignment
 const { countEvenNumbers } = require(`../scr/countEvenNumbers.js`);
-//Setup
-console.log(`Test 1: [1,2,3,4,5,6] => 3`);
-let input = [1, 2, 3, 4, 5, 6];
-let expected = 3;
+function testCountEvenNumbers() {
+    //Setup
+    const testCases = {
+        test1: {
+            input: [1, 2, 3, 4, 5, 6],
+            expected: 3,
+            logMessage: `Test 1: [1,2,3,4,5,6] => 3`
+        },
+        test2: {
+            input: [0, 1, 2],
+            expected: 2,
+            logMessage: `Test 2: [0,1,2] => 2`
+        },
+        test3: {
+            input: [1, 3, 5],
+            expected: 0,
+            logMessage: `Test 2: [0,1,2] => 2`
+        }
+    }
 
-//Execute
-let actual = countEvenNumbers(input);
+    //Execute && Verify
+    Object.entries(testCases).forEach(entry => {
+        console.log(entry[1].logMessage);
+        const actual = countEvenNumbers(entry[1].input);
+        const result = assertEquals(actual, entry[1].expected);
+        console.log(result);
+    })
 
-//Verify
-let result = assertEquals(actual, expected);
-console.log(result);
+}
 
-////////////////////////Test 1 end
-
-//Setup
-console.log(`Test 2: [0,1,2] => 2`);
-input = [0, 1, 2];
-expected = 2;
-
-//Execute
-actual = countEvenNumbers(input);
-
-//Verify
-result = assertEquals(actual, expected);
-console.log(result);
-
-////////////////////////Test 2 end
-
-//Setup 
-console.log(`Test 3: [1,3,5] => 0`);
-input = [1, 3, 5];
-expected = 0;
-
-// Execute 
-actual = countEvenNumbers(input);
-
-//Verify
-
-result = assertEquals(actual, expected);
-console.log(result);
-
-////////////////////////Test 3 end
+testCountEvenNumbers();
